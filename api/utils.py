e3e0f06317af6988f7b156d0e82dd939badf30e4
Zoubir <zoubir.ajaada@hotmail.fr>
2018-02-01 08:34:39 +0100

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -2,8 +2,12 @@
 from .models import Message
 
 def instantiate_reward_message(first_name, points):
-    text = 'Congratulations {}, you reach a total of {} points !'.format(
-        first_name,
-        points,
-    )
-    return Message(type=Message.ACTION, text=text)
+    if (points % 10) == 0:
+        text = 'Congratulations {}, you reach a total of {} points !'.format(
+            first_name,
+            points,
+            )
+
+        return Message(type=Message.ACTION, text=text)
+
+    return False
